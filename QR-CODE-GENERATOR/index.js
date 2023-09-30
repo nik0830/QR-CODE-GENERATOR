9514e40cd2c9e1cbc406e13ab0cfcd9316aa18bf
Nikhil Kumar <84836143+nik0830@users.noreply.github.com>
2023-10-01 03:26:54 +0530

    Add files via upload

---
+++ QR-CODE-GENERATOR/index.js
@@ -0,0 +1,26 @@
+let input = document.getElementById("inp");
+let img = document.getElementById("img");
+
+function myFun() {
+  let value = input.value;
+
+  if (value) {
+    img.style.border = "1px solid silver";
+    img.style.padding = "8px"
+    img.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${value}`
+
+
+
+    const refreshButton = document.getElementById("Refresh");
+
+
+    Refresh.addEventListener("click", function () {
+
+      location.reload();
+    });
+
+
+
+
+  }
+}
